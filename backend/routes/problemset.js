e15a826826ef0e6df577b5c08d998fa667a40756
SongDerrick <sd990307@gmail.com>
2023-03-09 18:08:10 +0900

    [FIX] Problem Set Modification2

--- backend/routes/problemset.js
+++ backend/routes/problemset.js
@@ -4,12 +4,20 @@ const axios = require('axios')
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./db/O2ARC.db');
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 /* GET users listing. */
 router.get('/:id', async function(req, res, next) {
 
     const userName = req.params.id
     var data
     var data2
+    var minirand = getRandomInt(5948,6096)
+    var rand = getRandomInt(6098,6496)
     console.log(userName)
 
     try{
@@ -25,10 +33,20 @@ router.get('/:id', async function(req, res, next) {
     res.render('problem_set', {
         userName: userName,
         miniARC_idlist: data,
-        ARC_idlist: data2
+        ARC_idlist: data2,
+        ran1: minirand,
+        ran2: rand
     })
 
 
 });
+
+router.get('/:id/:problem', function(req, res, next) {
+    const userName = req.params.id
+    const problem = req.params.problem
+
+    console.log(userName, problem)
+    res.send("HI")
+})
 
 module.exports = router;
