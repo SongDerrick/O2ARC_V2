2b979fe2762e646d796362f236ef39813335f1a6
SongDerrick <sd990307@gmail.com>
2023-04-24 17:37:36 +0900

    [SONG] tool bar updated

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/node_modules', express.static(path.join(__dirname + '/node_modules')))
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
