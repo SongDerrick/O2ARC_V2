e99cab7c18808bbb3f20aff5200d46c1b7882218
SongDerrick <sd990307@gmail.com>
2023-04-25 19:47:53 +0900

    [SONG] Grid Reset Function Done

--- backend/public/javascripts/jquery.js
+++ backend/public/javascripts/jquery.js
@@ -16,3 +16,22 @@ $(document).ready(function () {
     });
 
 })
+
+function resetOutputGrid() {
+    // Use jQuery to select all <div> elements with class "cell_final"
+    // and update their class attribute
+    $("#user_interact .cell_final").attr("class", "cell_final symbol_0");
+  }
+
+$("#resetBtn").on("click", function() {
+    // Call the resetOutputGrid() function when the button is clicked
+    resetOutputGrid();
+});
+
+function resizeOutputGrid() {
+    // Get the input value
+    var inputValue = $("#output_grid_size").val();
+    
+    // Log the input value to the console
+    console.log("Input Value:", inputValue);
+  }
