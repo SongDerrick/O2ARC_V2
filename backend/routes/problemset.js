5bb41a7c7414e38166f9b011f59c4d2d81f79dad
SongDerrick <sd990307@gmail.com>
2023-06-26 15:35:20 +0900

    [SONG] Spring Demo

--- backend/routes/problemset.js
+++ backend/routes/problemset.js
@@ -90,13 +90,13 @@ router.get('/:id/:problem', function(req, res, next) {
 
 
 
-  queryDB(query, params, (err, result) => {
-    if (err) {
-      return res.status(500).send(err);
-    } else {
-      return res.render('problem_solve', result);
-    }
-  });
+  // queryDB(query, params, (err, result) => {
+  //   if (err) {
+  //     return res.status(500).send(err);
+  //   } else {
+  //     return res.render('problem_solve', result);
+  //   }
+  // });
 });
 
 router.post('/:id/:problem/save-data', (req, res) => {
