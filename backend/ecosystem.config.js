fc3681a20df61cc85cc3240a05a9b872ef3b1e67
ConfeitoHS <confeitohs@gmail.com>
2023-07-18 23:07:49 +0900

    feat: added PM2 ecosystem setting

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    apps: [
+        {
+            name: 'app',
+            script: './bin/www',
+            instances: 0,
+            exec_mode: 'cluster',
+            wait_ready: true,
+            listen_timeout: 50000,
+            kill_timeout: 5000,
+            env: {
+                "NODE_ENV": "development",
+                "PORT": 3000
+            },
+            env_production: {
+                "NODE_ENV": "production",
+                "PORT": 80
+            },
+        }
+    ]
+  }
